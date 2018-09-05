c026e0f39c15b7bb19209b6b04ead15d6628333a
matt raffel <matt.raffel@evernym.com>
2018-09-05 12:52:08 -0600

    TOK-390 updates the crate name per review
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

--- tests/payment.rs
+++ tests/payment.rs
@@ -1,4 +1,4 @@
-extern crate rust_indy_sdk_wrapper as indy;
+extern crate rust_libindy_wrapper as indy;
 #[macro_use]
 mod utils;
 
@@ -9,7 +9,7 @@ mod low_tests {
     use super::*;
 
     #[test]
-    fn create_payment_address_works() {
+    fn create_payment_address_works () {
         let wallet_name = r#"{"id":"create_payment_address_works"}"#;
         safe_wallet_create!(wallet_name);
         let handle = Wallet::open(wallet_name, DEFAULT_CREDENTIALS).unwrap();
