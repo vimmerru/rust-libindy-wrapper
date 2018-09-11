dcd71497d372119935b1b3dfbaeb4e551f3d82e8
matt raffel <matt.raffel@evernym.com>
2018-09-11 08:36:47 -0600

    small changes
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

--- tests/utils/constants.rs
+++ tests/utils/constants.rs
@@ -1,8 +1,8 @@
 pub const DEFAULT_CREDENTIALS: &str = r#"{"key":""}"#;
-pub const TRUSTEE_SEED: &str = "000000000000000000000000Trustee1";
+pub const DID_1: &str = "VsKV7grR1BUE29mG2Fm2kX";
+pub const EXPORT_KEY: &str = "export_key";
 pub const MY1_SEED: &str = "00000000000000000000000000000My1";
 pub const PROTOCOL_VERSION: i32 = 2;
-pub const EXPORT_KEY: &str = "export_key";
 pub const SEED_1: &str = "00000000000000000000000000000My1";
+pub const TRUSTEE_SEED: &str = "000000000000000000000000Trustee1";
 pub const VERKEY_1: &str = "GjZWsBLgZCR18aL468JAT7w9CZRiBnpxUPPgyQxh4voa";
-pub const DID_1: &str = "VsKV7grR1BUE29mG2Fm2kX";
